e0e778c3508f2841e95c65ac57be02e130fdf556
Anton <anton.mois33v@gmail.com>
2019-04-08 12:48:55 -0400

    Exercise complete

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function arrayToObj() {
-   // your code goes here  
+function arrayToObj(array) {
+   // your code goes here
+   return array.reduce((acc, item) => {
+     acc[item[0]] = item[1];
+     return acc;
+   }, {});
 }
 
 module.exports = arrayToObj;
